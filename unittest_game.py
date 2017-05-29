aced1d9ab937af932ddaecfd02b6ea8232194b54
Greg Fernandez <fernangr@oregonstate.edu>
2017-05-29 14:23:27 -0400

    Completed Game.get_object_by_name() tests

--- unittest_game.py
+++ unittest_game.py
@@ -1,4 +1,5 @@
 import unittest
+from resources.game import Game
 from resources.player import Player
 from resources.item import Item
 from resources.space import Space
@@ -9,3 +10,26 @@ from auriga import Auriga
 
 class TestGame(unittest.TestCase):
     """Tests for game.py"""
+
+    def test_get_object_by_name(self):
+        rdm_player = Player(name="Rick")
+        rdm_item = Item(name="Portal Gun")
+        rdm_space = Space(name="Garage")
+        rdm_exit = Exit(name="Portal")
+        rdm_character = Character(name="Morty")
+        rdm_test_objects = [rdm_player, rdm_item, rdm_space, rdm_exit, rdm_character]
+        test_game = Game(rdm_player)
+
+        test_result = test_game.get_object_by_name(rdm_test_objects, rdm_player.name)
+        self.assertEqual(test_result, rdm_player)
+
+        test_result = test_game.get_object_by_name(rdm_test_objects, "Portal")
+        self.assertEqual(test_result, rdm_exit)
+        self.assertNotEqual(test_result, rdm_item)
+
+        test_result = test_game.get_object_by_name(rdm_test_objects, "Portal ")
+        self.assertNotEqual(test_result, rdm_item)
+        self.assertNotEqual(test_result, rdm_exit)
+
+if __name__ == "__main__":
+    unittest.main()
