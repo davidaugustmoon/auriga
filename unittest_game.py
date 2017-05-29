06668bf07fa9537b4e7f112018a5ea500da9cdcf
Greg Fernandez <fernangr@oregonstate.edu>
2017-05-29 13:23:06 -0400

    Starting Game class unit tests.

---
+++ unittest_game.py
@@ -0,0 +1,11 @@
+import unittest
+from resources.player import Player
+from resources.item import Item
+from resources.space import Space
+from resources.exit import Exit
+from resources.character import Character
+from auriga import Auriga
+
+
+class TestGame(unittest.TestCase):
+    """Tests for game.py"""
